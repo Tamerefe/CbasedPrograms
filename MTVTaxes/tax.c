76704fdfb10d0627349adf163a01dddbe58fede8
Tamer Akipek <tamerakipek@gmail.com>
2024-02-29 22:37:20 +0200

    v1.0.0

--- MTVTaxes/tax.c
+++ MTVTaxes/tax.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <string.h>
+
+struct car{
+    int payit;
+};
 
 int main(){
     
@@ -32,10 +37,77 @@ int main(){
 
     if (regis == 2){
         printf("Vehicle value excluding Kdv:");
-        printf("\n180.600Tl- / 180.600-316.400Tl / 316.400Tl+\n");
-        scanf("%d",&price);
+        if (engine <= 1300){
+            printf("\n180.600Tl- / 180.600-316.400Tl / 316.400Tl+\n");
+            scanf("%d",&price);
+        } else if (engine > 1300 || engine <= 1600){
+            printf("\n180.600Tl- / 180.600-316.400Tl / 316.400Tl+\n");
+            scanf("%d",&price);
+        } else if (engine > 1600 || engine <= 1800){
+            printf("\n452.800Tl- / 452.800Tl+\n");
+            scanf("%d",&price);
+        } else if (engine > 1800 || engine <= 2000){
+            printf("\n452.800Tl- / 452.800Tl+\n");
+            scanf("%d",&price);
+        } else if (engine > 2000 || engine <= 2500){
+            printf("\n565.500Tl- / 565.500Tl+\n");
+            scanf("%d",&price);
+        } else if (engine > 2500 || engine <= 3000){
+            printf("\n1.131.800Tl- / 1.131.800+\n");
+            scanf("%d",&price);
+        } else if (engine > 3000 || engine <= 3500){
+            printf("\n1.131.800Tl- / 1.131.800+\n");
+            scanf("%d",&price);
+        } else if (engine > 3500 || engine <= 4000){
+            printf("\n1.811.800Tl- / 1.811.800+\n");
+            scanf("%d",&price);
+        } else if (engine > 4000){
+            printf("\n2.151.400Tl- / 2.151.400+\n");
+            scanf("%d",&price);
+        }
+    }
+
+    switch (type){
+        case 'a':
+        case 'A':
+        if (age <= 3){
+            if (engine <= 1300){
+                /* code */
+            } else if (engine > 1300 || engine <= 1600){
+                /* code */
+            } else if (engine > 1600 || engine <= 1800){
+                
+            } else if (engine > 1800 || engine <= 2000){
+                /* code */
+            } else if (engine > 2000 || engine <= 2500){
+                /* code */
+            } else if (engine > 2500 || engine <= 3000){
+                /* code */
+            } else if (engine > 3000 || engine <= 3500){
+                /* code */
+            } else if (engine > 3500 || engine <= 4000){
+                /* code */
+            } else if (engine > 4000){
+                /* code */
+            }
+        } else if (age > 3 || age <= 6){
+            /* code */
+        } else if (age > 6 || age <= 11){
+            
+        } else if (age > 11 || age <= 15){
+            /* code */
+        } else if (age > 15){
+            /* code */
+        }
+        break;
+    default:
+        break;
     }
     
 
     return 0;
 }
+
+// https://hepiyi.com.tr/blog/mtv-nedir-mtv-hesaplama-nasil-yapilir
+
+// Sude ile evleneceğim - 29.02.2024 -
