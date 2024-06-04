c96f3632da37810b0e10dc4f53d2910968a534cd
Tamer Akipek <tamerakipek@gmail.com>
2024-06-04 15:35:42 +0300

    v1.1.1

---
+++ Rainbow/color.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+    
+    printf("Rainbow CMD Page");
+    system("color 4F");
+    Sleep(1000);
+    system("color 6F");
+    Sleep(1000);
+    system("color 2F");
+    Sleep(1000);
+    system("color 3F");
+    Sleep(1000);
+    system("color 1F");
+    Sleep(1000);
+    system("color 5F");
+    Sleep(1000);
+    system("color 0F");
+    Sleep(1000);
+    
+    return 0;
+}
